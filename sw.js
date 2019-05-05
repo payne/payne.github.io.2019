160c9cc9fe335191f54f919739cb56eb60a329cb
Matt Payne <Payne@MattPayne.org>
2019-05-04 21:11:03 -0500

    cp -r public/* .

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-804c9c720eed4efa4e9f.js"
+    "url": "webpack-runtime-ffc25cb4a3a89170759a.js"
   },
   {
     "url": "styles.86bcfd0d2f8fe429f406.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "da8a44e1017ef437f5239b20eeb3efa9"
+    "revision": "52f940a2627714708308dd7b26039ce5"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
